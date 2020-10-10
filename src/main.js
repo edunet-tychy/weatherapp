774ab00376895fc72447ec4d186692b06317cff1
Grzegorz Szymkowiak <szymkowiak.grzegorz@gmail.com>
2020-10-10 09:22:22 +0200

    Added validation to search input and error handling

--- src/main.js
+++ src/main.js
@@ -29,6 +29,10 @@ class WeatherApp{
             let query = this.viewElems.searchInput.value;
             getWeatherByCity(query).then(data => {
                 this.displayWeatherData(data);
+                this.viewElems.searchInput.style.borderColor = 'black';
+            }).catch(() => {
+                this.fadeInOut();
+                this.viewElems.searchInput.style.borderColor = 'red';
             });
         }
     }
